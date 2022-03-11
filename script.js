a14d3581250085978bccef20a58ab1a057c245fa
khan <khanetmoi@gmail.com>
2022-03-11 07:58:20 +0000

    mobile menu- toggle button

--- script.js
+++ script.js
@@ -1,8 +1,35 @@
-function showMenu() {
-  const htmlShow = document.getElementById('WebsitePages');
-  if (htmlShow.style.display === 'none') {
-    htmlShow.style.display = 'block';
+ function showMenu() {
+   const htmlShow = document.querySelectorAll(".headvisible");
+   const toggle  = document.getElementById('toggle')
+   const john = document.getElementById('john')
+   const bgImage = document.getElementById('eposition')
+   const eposition = document.getElementById('eposition')
+   const tg = document.getElementsByClassName('tattempt')[0]
+ 
+  
+for (var i = 0; i<= htmlShow.length;i++)
+{
+  if (htmlShow[i].style.display === 'none') {
+    htmlShow[i].style.display = 'flex';
+    toggle.innerHTML = 'X'
+    john.innerHTML = ''
+    bgImage.style.backgroundImage = "url('img/toggle.png')"
+    //bgImage.style.backgroundRepeat = "no-repeat"
+    //bgImage.style.backgroundSize = "cover"
+      bgImage.style.backgroundSize = 'cover'
+    eposition.style.position = 'fixed'
+    eposition.style.height = "812px";
+    eposition.style.zIndex = 10
+    eposition.style.backgroundColor = "#191765"
+
   } else {
-    htmlShow.style.display = 'none';
+    htmlShow[i].style.display = 'none';
+    toggle.innerHTML = tg.innerHTML;
+    john.innerHTML = 'John Doe'
+    bgImage.style.backgroundImage = "url('#')"
+    eposition.style.position = 'relative'
+    eposition.style.height = "10px";
   }
+}
+
 }
